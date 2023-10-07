f46a7d5bc105b4d6ea2e90e0e69ef6b4a87045e4
Anastasiia Ziablova <ziablova25@gmail.com>
2023-10-07 14:30:49 +0300

    HW #4: Commit 2:Fixed some branches of "if-else"

--- js/script.js
+++ js/script.js
@@ -5,23 +5,33 @@ let ageOfNumberType = Number(age);
 //console.log(typeof ageOfNumberType, ageOfNumberType);
 
 let answer = ageOfNumberType % 10;
-
+//console.log(answer);
 /*Була ідея зробити перевірку на те, що якщо age - число, то далі перевіряємо, щоб вірно показувалось рік/роки/років.
 Якщо не число, то виводити повідомлення про це.
 Але у prompt ми отримуємо завжди строку, тому така перевірка виявилась непотрібною.
 Тобто, я все одно перетворюю дану строку на число.*/
 
+if (isNaN(ageOfNumberType)){ //Якщо я правильно зрозуміла, то помилка через те, що виводило "Вам NaN років!"
+    console.log("До побачення!");
+}
+else if ((age >=11 && age <= 14) || (age >=111 && age <= 114)){ //вважаю, понад 200 років робити не треба
+    console.log("Вам " + age + " років!");
+}
+else{
     if (answer === 1) {
         console.log("Вам " + age + " рік!");
-    } else if (answer === 2 || answer === 3 || answer === 4) {
+    } else if (answer >= 2 && answer <= 4) {
         console.log("Вам " + age + " роки!");
-    } else if (answer === 5 || answer === 6 || answer === 7 || answer === 8 || answer === 9 || answer === 10) {
+    } else if (answer === 0 || (answer >= 5 && answer <= 9)) {
         console.log("Вам " + age + " років!");
-    } else if(answer <= 0){
+    } else if(answer < 0){
         console.log("Ви або ще не народились, або помилилися. Будь ласка, введіть свій справжній вік.");
     } else {
         console.log("Ви ввели не число. Будь ласка, введіть свій рік.");
     }
+}
+
+
 
 
 
