0a7d93c60446dce6ae96abfd97de5683a8a7a397
Anastasiia Ziablova <ziablova25@gmail.com>
2023-10-19 16:26:24 +0300

    HW #7: Code review

--- myIsNan.js
+++ myIsNan.js
@@ -6,38 +6,11 @@ function myIsNaN (item) {
     if (item !== item){
         return true;
     }
-    else if (isFinite(item) || item === null || typeof item === "boolean" || typeof item === "number") {
-        return false;
-    }
-    else {
-        return true;
-    }
+    else return !(item === null || typeof item === "boolean" || typeof item === "number");
 }
-console.log("NaN is " + myIsNaN(NaN));
 
-
-
-/*
-//My test
-let person = {
-    id: 10,
-    age: 20,
-}
-let i = null;
-
-console.log("4 " + myIsNaN(4));
-console.log("2+3 " + myIsNaN(2+3));
-console.log("5.2 " + myIsNaN(5.2));
-console.log("-10 " + myIsNaN(-10));
-console.log("0 " + myIsNaN(0));
-
-console.log("NaN is " + myIsNaN(NaN));
-console.log("Undefined is " + myIsNaN(undefined));
-console.log("Infinity is " + myIsNaN(Infinity));
-console.log("Infinity 1e500 is " + myIsNaN(1e500));
-console.log("String is " + myIsNaN("String"));
-console.log("Boolean is " + myIsNaN(false));
-console.log("Object is " + myIsNaN(person));
-console.log("Null is " + myIsNaN(null));
-console.log("Symbol is " + myIsNaN('*'));
-console.log("3+5 " + myIsNaN("3+5"));*/
+console.log(myIsNaN(45));
+console.log(myIsNaN(undefined));
+console.log(myIsNaN(NaN));
+console.log(myIsNaN("String"));
+console.log(myIsNaN(true));
